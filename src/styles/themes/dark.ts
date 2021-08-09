9986220b7413e6a5d2bce80f989653a3b7e3d175
leandrosimonato <leandrosimonato@gmail.com>
2021-08-09 17:31:48 -0300

    Módulo interiro da Aplicação.
    Signed-off-by: leandrosimonato <leandrosimonato@gmail.com>

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secundary: '#C62E65',
+
+    background: '#222',
+    text: '#fff'
+  },
+};
